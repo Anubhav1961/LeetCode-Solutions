499c1fa44ccfaff3894d47998127d82700d8a570
Anubhav Dubey <anubhavdubey1961@gmail.com>
2026-09-18 17:41:15 +0530

    Time: 32 ms (36.84%), Space: 23.6 MB (41.52%) - LeetHub

---
+++ 1520-maximum-number-of-non-overlapping-substrings/1520-maximum-number-of-non-overlapping-substrings.cpp
@@ -0,0 +1,73 @@
+class Solution {
+public:
+    vector<string> maxNumOfSubstrings(string s) {
+        
+        unordered_map<char, pair<int, int>> mp;
+        // first and last occurrence ko store kara
+        for (int i = 0; i < s.size(); i++) {
+            char ch = s[i];
+
+            if (mp.find(ch) == mp.end()) {
+                mp[ch] = {i, i};
+            } 
+            else {
+                mp[ch].second = i;
+            }
+        }
+
+        vector<pair<int, int>> intervals;
+
+        // Create valid intervals
+        for (auto it : mp) {
+
+            char ch = it.first;
+
+            int l = it.second.first;
+            int r = it.second.second;
+
+            bool valid = true;
+
+            for (int i = l; i <= r; i++) {
+
+                char x = s[i];
+
+                // x ka koi occurrence l se pehle hai
+                if (mp[x].first < l) {
+                    valid = false;
+                    break;
+                }
+
+                // interval ko expand karo
+                r = max(r, mp[x].second);
+            }
+
+            if (valid) {
+                intervals.push_back({l, r});
+            }
+        }
+
+        // End position ke according sort
+        sort(intervals.begin(), intervals.end(),
+             [](pair<int,int> a, pair<int,int> b) {
+                 return a.second < b.second;
+             });
+
+        vector<string> ans;
+
+        int prevEnd = -1;
+
+        // Greedy
+        for (auto p : intervals) {
+
+            int l = p.first;
+            int r = p.second;
+
+            if (l > prevEnd) {
+                ans.push_back(s.substr(l, r - l + 1));
+                prevEnd = r;
+            }
+        }
+
+        return ans;
+    }
+};
